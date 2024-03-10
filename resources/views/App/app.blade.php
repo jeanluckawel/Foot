ed1b401e7af56b1aecb48cc096eaa6a86959c1a2
Jean luc kawel <20ka208@esisalama.org>
2024-03-10 22:00:22 +0200

    New branch

---
+++ resources/views/App/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="En">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>@yield('title')</title>
+</head>
+<body>
+
+</body>
+</html>
